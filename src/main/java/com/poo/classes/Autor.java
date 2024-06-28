025c2c8d949f58e8f80c8dde64438308a2f16342
Giordano Borsato <borsatogiordano@gmail.com>
2024-06-28 20:16:01 -0300

    Get e Set Class Autor

--- src/main/java/com/poo/classes/Autor.java
+++ src/main/java/com/poo/classes/Autor.java
@@ -1,6 +1,23 @@
 package com.poo.classes;
 
 public class Autor {
-    int autorID;
-    String nome;
+    private int autorID;
+    private String nome;
+
+    public int getAutorID() {
+        return autorID;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+
+    public void setAutorID(int autorID) {
+        this.autorID = autorID;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
