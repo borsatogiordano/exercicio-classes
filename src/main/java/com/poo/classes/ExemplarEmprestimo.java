6b886b494e05698152c99bcdd63dd08ad637d310
Giordano Borsato <borsatogiordano@gmail.com>
2024-06-28 21:18:26 -0300

    Get e Set Class Exemplar Emprestimo

--- src/main/java/com/poo/classes/ExemplarEmprestimo.java
+++ src/main/java/com/poo/classes/ExemplarEmprestimo.java
@@ -1,8 +1,41 @@
 package com.poo.classes;
 
 public class ExemplarEmprestimo {
-        private int ExemplarEmprestimoID;
+        private int exemplarEmprestimoID;
         private Exemplar exemplar;
         private Emprestimo emprestimo;
         private Devolucao devolucao;
+
+        public int getExemplarEmprestimoID() {
+                return exemplarEmprestimoID;
+        }
+
+        public Exemplar getExemplar() {
+                return exemplar;
+        }
+
+        public Emprestimo getEmprestimo() {
+                return emprestimo;
+        }
+
+        public Devolucao getDevolucao() {
+                return devolucao;
+        }
+
+        public void setExemplarEmprestimoID(int exemplarEmprestimoID) {
+                this.exemplarEmprestimoID = exemplarEmprestimoID;
+        }
+
+        public void setExemplar(Exemplar exemplar) {
+                this.exemplar = exemplar;
+        }
+
+        public void setEmprestimo(Emprestimo emprestimo) {
+                this.emprestimo = emprestimo;
+        }
+
+        public void setDevolucao(Devolucao devolucao) {
+                this.devolucao = devolucao;
+        }
+
 }
