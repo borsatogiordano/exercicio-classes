b33258409aef46f8ad5087d2466c864feb27385b
Giordano Borsato <borsatogiordano@gmail.com>
2024-06-28 21:09:38 -0300

    Get e Set Class Livro

--- src/main/java/com/poo/classes/Exemplar.java
+++ src/main/java/com/poo/classes/Exemplar.java
@@ -3,4 +3,20 @@ package com.poo.classes;
 public class Exemplar {
     int exemplarID;
     Livro livro;
+
+    public int getExemplarID() {
+        return exemplarID;
+    }
+
+    public Livro livro() {
+        return livro;
+    }
+
+    public void setExemplarID(int exemplarID){
+        this.exemplarID = exemplarID;
+    }
+
+    public void setLivro(Livro livro){
+        this.livro = livro;
+    }
 }
