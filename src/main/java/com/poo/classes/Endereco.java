d699e4709e978b471e0f798856b8535d1388e98d
Giordano Borsato <borsatogiordano@gmail.com>
2024-06-28 21:06:23 -0300

    Get e Set Class Endereco

--- src/main/java/com/poo/classes/Endereco.java
+++ src/main/java/com/poo/classes/Endereco.java
@@ -1,10 +1,71 @@
 package com.poo.classes;
 
 public class Endereco {
-    int enderecoID;
-    String uf;
-    String cidade;
-    String tipo;
-    int numeroResidencia;
-    String complemento;
+    private Pessoa pessoa;
+    private int enderecoID;
+    private String uf;
+    private String cidade;
+    private String tipo;
+    private int numeroResidencia;
+    private String complemento;
+
+    public Pessoa getPessoa() {
+        return pessoa;
+    }
+
+    public int getEnderecoID() {
+        return enderecoID;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public int getNumeroResidencia() {
+        return numeroResidencia;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+
+    public void setEnderecoID(int enderecoID) {
+        this.enderecoID = enderecoID;
+    }
+
+    public void setUf(String uf) {
+        if (uf != null && uf.length() == 2) {
+            this.uf = uf.toUpperCase();
+        } else {
+            throw new IllegalArgumentException("UF deve ter exatamente 2 caraceteres.");
+        }
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setNumeroResidencia(int numeroResidencia) {
+        this.numeroResidencia = numeroResidencia;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
 }
