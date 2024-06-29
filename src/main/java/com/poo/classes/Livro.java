5a7aded0d107b6b47e3ea276e85b0e6f3c302298
Giordano Borsato <borsatogiordano@gmail.com>
2024-06-28 21:20:36 -0300

    Get e Set Class Livro

--- src/main/java/com/poo/classes/Livro.java
+++ src/main/java/com/poo/classes/Livro.java
@@ -7,4 +7,51 @@ public class Livro {
     private int ano;
     private Editora editora;
     private Autor autor;
+
+    public int getLivroID() {
+        return livroID;
+    }
+    public String getEdicao() {
+        return edicao;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public Editora getEditora() {
+        return editora;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public void setLivroID(int livroID) {
+        this.livroID = livroID;
+    }
+
+    public void setEdicao(String edicao) {
+        this.edicao = edicao;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public void setEditora(Editora editora) {
+        this.editora = editora;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
 }
