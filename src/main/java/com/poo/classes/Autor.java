342c131bff2afdda3bad9ee3978242a1f25ccaa6
Giordano Borsato <borsatogiordano@gmail.com>
2024-07-02 21:15:16 -0300

    Construtores Class Autor

--- src/main/java/com/poo/classes/Autor.java
+++ src/main/java/com/poo/classes/Autor.java
@@ -1,14 +1,21 @@
 package com.poo.classes;
 
 public class Autor {
+    private static int contador = 0;
     private int autorID;
     private String nome;
+
+    public Autor(String nome, int autorID) {
+        this.nome = nome;
+        contador = autorID;
+        contador++;
+    }
 
     public int getAutorID() {
         return autorID;
     }
 
-    public String getNome(){
+    public String getNome() {
         return nome;
     }
 
